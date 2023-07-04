ef91afe5fd17d6587f1f8f63900a805438671f6f
Alice (DegtyarevAS) <victor2017selvo@mail.ru>
2023-07-04 10:29:36 +0300

    code: добавлен модульный тест для выбора цвета

--- lab03_test/test.cpp
+++ lab03_test/test.cpp
@@ -3,50 +3,17 @@
 
 #include <cassert>
 
-void test_positive() {
-    double min = 0;
-    double max = 0;
-    find_minmax({ 1, 2, 3 }, min, max);
-    assert(min == 1);
-    assert(max == 3);
+void check_red_color() {
+    bool answer = check_color("red");
+    assert(answer == false);
 }
 
-void test_negative() {
-    double min = 0;
-    double max = 0;
-    find_minmax({ -1, -2, -3 }, min, max);
-    assert(min == -3);
-    assert(max == -1);
-}
-
-void test_same_numbers() {
-    double min = 0;
-    double max = 0;
-    find_minmax({ 1, 1, 1 }, min, max);
-    assert(min == 1);
-    assert(max == 1);
-}
-
-void test_one_elem() {
-    double min = 0;
-    double max = 0;
-    find_minmax({ 1 }, min, max);
-    assert(min == 1);
-    assert(max == 1);
-}
-
-void test_empty() {
-    double min = 0;
-    double max = 0;
-    find_minmax({}, min, max);
-    assert(min == 0);
-    assert(max == 0);
+void check_blue_color() {
+    bool answer = check_color("blue");
+    assert(answer == true);
 }
 
 int main() {
-    test_positive();
-    test_negative();
-    test_same_numbers();
-    test_one_elem();
-    test_empty();
+    check_red_color();
+    check_blue_color();
 }
