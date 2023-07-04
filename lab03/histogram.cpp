f706b5381e5f1b90f67a97ef2c17f67ef9eedd81
Alice (DegtyarevAS) <victor2017selvo@mail.ru>
2023-07-04 10:18:50 +0300

    code: добавлен файл реализации

---
+++ lab03/histogram.cpp
@@ -0,0 +1,17 @@
+
+#include "histogram.h"
+
+void find_minmax(const vector<double>& numbers, double& min, double& max) {
+    if (numbers.size() != 0) {
+        min = numbers[0];
+        max = numbers[0];
+        for (double x : numbers) {
+            if (x < min) {
+                min = x;
+            }
+            else if (x > max) {
+                max = x;
+            }
+        }
+    }
+}
